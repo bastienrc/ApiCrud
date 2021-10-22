b87844bafa1f7771601377328752adc2791238a8
Sebastien Colbert <colbertsebastien@gmail.com>
2021-10-22 16:16:22 +0200

    Add Menu + Fx Delete

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const img = document.querySelector('#illustrator img')
 //       document.getElementById('erreur').innerHTML = "Erreur: :("
 //     }
 //   })
+const iconNew = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+</svg>`
 
 function listProfilsUsersInUsersContainer(data) {
   const usersContainer = document.querySelector('#usersContainer')
+  const iconTrash = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+  </svg>`
+  const iconEdit = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+  </svg>`
 
   usersContainer.innerHTML = ''
   data.data.forEach(({email, first_name, last_name, avatar, id}) => {
     usersContainer.innerHTML += `
-      <div class="userProfile">
+      <div class="userProfile" id="id-${id}">
+        <div class="id">${id}</div>
         <div class="avatar">
           <img src="${avatar}" alt="avatar de l'utilisateur ${first_name} ${last_name}" />
         </div>
@@ -24,11 +36,27 @@ function listProfilsUsersInUsersContainer(data) {
           <p>${first_name} ${last_name}</p>
           <p>${email}</p>
         </div>
-        <div class="id">${id}</div>
+        <div class="menu">
+          <span title="Modifier">${iconEdit}</span>
+          <span title="Supprimer" onClick="deleteUser(${id})">${iconTrash}</span>
+        </div>
       </div>
     `
   })
 }
+
+function deleteUser(userId) {
+  console.log(`https://reqres.in/api/users/${userId}`)
+  fetch(`https://reqres.in/api/users/${userId}`, { method: 'DELETE' })
+  .then(response => {
+    if(response.ok){
+      console.log('DELETE STATUS CODE ' + response.status)
+    } else {
+      console.log('DELETE STATUS CODE ' + response.status)
+    }
+  })
+}
+
 
 // https://reqres.in
 let users = {}
