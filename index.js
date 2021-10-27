c2c91a75a6b17f3ea6da6fa2be88dcdf732c5457
Sebastien Colbert <colbertsebastien@gmail.com>
2021-10-27 11:46:20 +0200

    Refactoring + view response POST

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// https://thecatapi.com
+// https://thecatapi.com => easter egg
 document.getElementById('cat').addEventListener('click', e => {
   fetch('https://api.thecatapi.com/v1/images/search')
     .then(response => {
@@ -11,20 +11,9 @@ document.getElementById('cat').addEventListener('click', e => {
     })
 })
 
-document.getElementById('addUser').addEventListener('click', e => {
-  if (document.getElementById('addUser').classList.contains('active')) {
-    document.getElementById('formContainer').style = 'display: none'
-    document.getElementById('usersContainer').style = ''
-    document.getElementById('addUser').innerHTML = '<i class="fas fa-user-plus"></i>'
-  } else {
-    document.getElementById('formContainer').style = 'display: block'
-    document.getElementById('usersContainer').style = 'display: none'
-    document.getElementById('addUser').innerHTML = '<i class="fas fa-times"></i>'
-  }
-  document.getElementById('addUser').classList.toggle('active')
-})
 
-function listProfilsUsersInUsersContainer(data) {
+// Functions
+function listProfilsUsersInUsersContainer (data) {
   const usersContainer = document.querySelector('#usersContainer')
   usersContainer.innerHTML = ''
   data.data.forEach(({email, first_name, last_name, avatar, id}) => {
@@ -47,7 +36,7 @@ function listProfilsUsersInUsersContainer(data) {
   })
 }
 
-function deleteUser(userId) {
+function deleteUser (userId) {
   console.log(`https://reqres.in/api/users/${userId}`)
   fetch(`https://reqres.in/api/users/${userId}`, { method: 'DELETE' })
   .then(response => {
@@ -60,11 +49,74 @@ function deleteUser(userId) {
   })
 }
 
+function updateUser () {
+  console.log(`https://reqres.in/api/users/${userId}`)
+  fetch(`https://reqres.in/api/users/${userId}`, {
+    method: 'PUT'
+  })
+  .then(response => {
+    if(response.ok){
+      console.log('DELETE STATUS CODE ' + response.status)
+      document.getElementById(`id-${userId}`).style.display = 'none'
+    } else {
+      console.log('DELETE STATUS CODE ' + response.status)
+    }
+  })
+}
+
+function readAllUsers () {
+  let users = {}
+  fetch('https://reqres.in/api/users?per_page=12')
+    .then(response => {
+      document.getElementById('response').innerHTML = `<pre>Status: ${response.status}</pre>`
+      if(response.ok){
+        response.json().then(data => {
+          listProfilsUsersInUsersContainer(data)
+          document.getElementById('response').innerHTML += `<pre>${JSON.stringify(data.data, null, 2)}</pre>`
+          console.log(data.data)
+      })
+      } else {
+        console.log('ERREUR !!!')
+      }
+    })
+}
+
+
+// Event
+// Bouton view response (en sous-titre)
+const btnViewResponse = document.getElementById('btnViewResponse')
+btnViewResponse.addEventListener('click', () => {
+  if (btnViewResponse.classList.contains('active')) {
+    document.getElementById('response').style = 'display: none'
+    document.getElementById('usersContainer').style = 'display: flex'
+    btnViewResponse.innerHTML = 'Voir la response'
+  } else {
+    document.getElementById('response').style = 'display: block'
+    document.getElementById('usersContainer').style = 'display: none'
+    btnViewResponse.innerHTML = 'Fermer la response'
+  }
+  btnViewResponse.classList.toggle('active')
+})
+
+// Bouton add user (en haut à droite)
+const addUser = document.getElementById('addUser')
+addUser.addEventListener('click', () => {
+  if (addUser.classList.contains('active')) {
+    document.getElementById('formContainer').style = 'display: none'
+    document.getElementById('usersContainer').style = ''
+    addUser.innerHTML = '<i class="fas fa-user-plus"></i>'
+  } else {
+    document.getElementById('formContainer').style = 'display: block'
+    document.getElementById('usersContainer').style = 'display: none'
+    addUser.innerHTML = '<i class="fas fa-times"></i>'
+  }
+  addUser.classList.toggle('active')
+})
+
+// Envoie du formulaire
 const form = document.getElementById("addForm");
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-
-
 
   let submitForm = {}
   Array.from(new FormData(form), (entry) => {
@@ -96,46 +148,21 @@ form.addEventListener('submit', (e) => {
   document.getElementById('addUser').classList.toggle('active')
 })
 
-function updateUser() {
-  console.log(`https://reqres.in/api/users/${userId}`)
-  fetch(`https://reqres.in/api/users/${userId}`, {
-    method: 'PUT'
-  })
-  .then(response => {
-    if(response.ok){
-      console.log('DELETE STATUS CODE ' + response.status)
-      document.getElementById(`id-${userId}`).style.display = 'none'
-    } else {
-      console.log('DELETE STATUS CODE ' + response.status)
-    }
-  })
-}
 
-function readAllUsers() {
-  let users = {}
-  fetch('https://reqres.in/api/users?page=1')
-    .then(response => {
-      if(response.ok){
-        response.json().then(data => listProfilsUsersInUsersContainer(data))
-      } else {
-        console.log('ERREUR !!!')
-      }
-    })
-}
-
+// Init
 readAllUsers()
 
-  /* Je rccupére les données du formulaire */
-  const avatar = document.getElementById("avatar")
-  const imgPreview = document.getElementById("img-preview")
-  avatar.addEventListener("change", function () {
-    const files = avatar.files[0]
-    if (files) {
-      const fileReader = new FileReader()
-      fileReader.readAsDataURL(files)
-      fileReader.addEventListener("load", function () {
-        imgPreview.style.display = "block"
-        imgPreview.innerHTML = '<img src="' + this.result + '" />'
-      })
-    }
-  })
+/* Je récupére les données du formulaire */
+const avatar = document.getElementById("avatar")
+const imgPreview = document.getElementById("img-preview")
+avatar.addEventListener("change", function () {
+  const files = avatar.files[0]
+  if (files) {
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(files)
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block"
+      imgPreview.innerHTML = '<img src="' + this.result + '" />'
+    })
+  }
+})
